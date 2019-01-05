5906acd1b57317b11cd20d4df0ba1ac4fc585aa5
mrhili <mohamedaminemrhili@users.noreply.github.com>
2019-01-05 17:04:11 +0100

    Update regression.py

--- regression.py
+++ regression.py
@@ -24,6 +24,16 @@ def best_fit_slope_intercept(xs,ys):
     b= mean(ys) - m*mean(xs)
     return m,b
 
+
+def squared_error(ys_orig,ys_line):
+    return sum( (ys_line - ys_orig) **2 )
+    
+def coefficient_of_determination(ys_orig,ys_line):
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig,ys_line)
+    squared_error_y_mean = squared_error(ys_orig,y_mean_line)
+    return 1 - ( squared_error_regr / squared_error_y_mean)
+
 m,b = best_fit_slope_intercept(xs,ys)
 
 regression_line = [(m*x)+b for x in xs]
@@ -32,7 +42,7 @@ regression_line = [(m*x)+b for x in xs]
 predict_x = 8
 predict_y = (m*predict_x)+b
 
-
+r_squared = coefficient_of_determination(ys, regression_line)
 plt.scatter(xs,ys)
 plt.scatter(predict_x,predict_y, color='g')
 plt.plot(xs,regression_line)
